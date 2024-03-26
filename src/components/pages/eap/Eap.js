e2e8f13540543e43f9fc0c9ae23add530614ff85
Tapojita Kar <121812218+Tapo41@users.noreply.github.com>
2024-03-27 00:22:57 +0530

    Update Eap.js

--- src/components/pages/eap/Eap.js
+++ src/components/pages/eap/Eap.js
@@ -16,8 +16,10 @@ const Eap = () => {
         <>
             {isLoading && <CustomLoader />}
                 
+            <!-- Calendly inline widget begin -->
             <div class="calendly-inline-widget" data-url="https://calendly.com/karbiswa098/30min" style="min-width:320px;height:700px;"></div>
             <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+            <!-- Calendly inline widget end -->
         </>
     );
 };
